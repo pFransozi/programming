82c7c480f0f4b49d60d478fb225ebcb083a64048
Philipe Fransozi <philipehf@gmail.com>
2022-10-06 15:50:30 -0300

    add projects from chapter 7.

---
+++ project-architecture-principles-design-patterns-.NET6-cs/s02-07_Strategy/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Strategy.Models;
+using Strategy.Services;
+using System.Diagnostics;
+
+namespace Strategy.Controllers;
+
+public class HomeController : Controller
+{
+    /*
+    The use of private readonly fields is beneficial for two reasons:
+        • because private, it is not expose outside of the class (encapsulation).
+        • because readonly, only set the value during the initialization; usually only once.
+            In the case of constructor injection, this ensures that the injected dependency, referenced by
+            the private field, cannot be changed by other parts of the class.
+    */
+    private readonly IHomeService _homeService;
+    public HomeController(IHomeService homeService)
+    {
+        _homeService = homeService ?? throw new ArgumentNullException(nameof(homeService));
+    }
+
+    public IActionResult Index()
+    {
+        var data = _homeService.GetHomePageData();
+        var viewModel = new HomePageViewModel(data);
+        return View(viewModel);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ project-architecture-principles-design-patterns-.NET6-cs/s02-07_Strategy/Program.cs
@@ -0,0 +1,37 @@
+﻿using Strategy.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Now, if we rerun the app, the home page should load. That tells ASP.NET to inject a HomeService 
+//instance when a class depends on the IHomeService interface.
+builder.Services.AddSingleton<IHomeService, HomeService>();
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCookiePolicy();
+app.UseRouting();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+app.Run();
